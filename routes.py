7370aa2470c10cf81e37387dddae11cdaf6b33b1
Ankit Pal <ankit.pal@unthinkable.co>
2021-11-03 19:47:37 +0530

    Error handling and localization added

--- routes.py
+++ routes.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi.params import Depends
-from schemas.post import PostCreate, PostUpdate
+from schemas.post import PostCreate, PostUpdate, Post as PostDisplay
 from sqlalchemy.orm import Session
 from models.post import Post as PostModel
 from db.session import session_local
+from fastapi_localization import TranslateJsonResponse, lazy_gettext as _
+from typing import List
 
 router = APIRouter()
 
@@ -16,39 +18,60 @@ def get_db():
         db.close()
 
 
-@router.get("/")
-def display_all_posts(db: Session = Depends(get_db)):
-    posts = db.query(PostModel).all()
-    return posts
+# @router.get("/", response_class=TranslateJsonResponse, response_model=List[PostDisplay])
+# def display_all_posts(db: Session = Depends(get_db)):
+#     posts = db.query(PostModel).all()
+#     if not posts:
+#         raise HTTPException(status_code=404, detail=_("NO POSTS CREATED YET"))
+#     return posts
+
+
+# @router.post("/posts", response_class=TranslateJsonResponse, response_model=PostDisplay)
+# def create_post(request: PostCreate, db: Session = Depends(get_db)):
+#     post = PostModel(title=request.title, content=request.content)
+#     if not post:
+#         raise HTTPException(status_code=500, detail=_("INTERNAL SERVER ERROR"))
+#     db.add(post)
+#     db.commit()
+#     db.refresh(post)
+#     return post
+
+
+# @router.get("/posts/{id}", response_class=TranslateJsonResponse, response_model=PostDisplay)
+# def read_post(id: int, db: Session = Depends(get_db)):
+#     post = db.query(PostModel).filter(PostModel.id == id).first()
+#     if not post:
+#         raise HTTPException(status_code=404, detail=_(f"NO POST FOUND WITH ID: {id}"))
+#     return post
+
+
+# @router.put("/posts/{id}", response_class=TranslateJsonResponse, response_model=PostDisplay)
+# def update_post(id: int, request: PostUpdate, db: Session = Depends(get_db)):
+#     post = db.query(PostModel).filter(PostModel.id == id).first()
+#     if not post:
+#         raise HTTPException(status_code=404, detail=_(f"NO POST FOUND WITH ID: {id}"))
+#     post.title = request.title if request.title else post.title
+#     post.content = request.content if request.content else post.content
+#     db.commit()
+#     return post
+
+
+# @router.delete("/posts/{id}", response_class=TranslateJsonResponse, response_model=PostDisplay)
+# def delete_post(id: int, db: Session = Depends(get_db)):
+#     post = db.query(PostModel).filter(PostModel.id == id).first()
+#     if not post:
+#         raise HTTPException(status_code=404, detail=_(f"NO POST FOUND WITH ID: {id}"))
+#     db.delete(post)
+#     db.commit()
+#     return post
 
 
 @router.post("/posts")
 def create_post(request: PostCreate, db: Session = Depends(get_db)):
     post = PostModel(title=request.title, content=request.content)
+    if not post:
+        raise HTTPException(status_code=500, detail="INTERNAL SERVER ERROR")
     db.add(post)
     db.commit()
     db.refresh(post)
-    return post
-
-
-@router.get("/posts/{id}")
-def read_post(id: int, db: Session = Depends(get_db)):
-    post = db.query(PostModel).filter(PostModel.id == id).first()
-    return post
-
-
-@router.put("/posts/{id}")
-def update_post(id: int, request: PostUpdate, db: Session = Depends(get_db)):
-    post = db.query(PostModel).filter(PostModel.id == id).first()
-    post.title = request.title if request.title else post.title
-    post.content = request.content if request.content else post.content
-    db.commit()
-    return post
-
-
-@router.delete("/posts/{id}")
-def delete_post(id: int, db: Session = Depends(get_db)):
-    post = db.query(PostModel).filter(PostModel.id == id).first()
-    db.delete(post)
-    db.commit()
     return post
--- schemas/post.py
+++ schemas/post.py
@@ -1,10 +1,11 @@
-from typing import Optional
-from pydantic import BaseModel
+from fastapi.param_functions import File
+from pydantic import BaseModel, Field
+from fastapi_localization import TranslatableStringField
 
 
 class PostBase(BaseModel):
-    title: str
-    content: Optional[str] = None
+    title: str = Field(..., max_length=200)
+    content: str = Field(None, max_length=1000)
 
 
 class PostCreate(PostBase):
@@ -12,11 +13,13 @@ class PostCreate(PostBase):
 
 
 class PostUpdate(PostBase):
-    title: Optional[str] = None
+    title: str = Field(None, max_length=200)
 
 
-class Post(PostBase):
-    date_posted: str
+class Post(BaseModel):
+    title: TranslatableStringField
+    content: TranslatableStringField
+    date_posted: str = Field(None)
 
-    class config:
+    class Config:
         orm_mode = True
